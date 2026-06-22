990c63242a46b637cb0bf2856697300823369ff7
pranav718 <raypranav718@gmail.com>
2026-06-22 17:22:51 +0530

    replace flip w wind woosh

--- src/components/3d/Bookshelf.tsx
+++ src/components/3d/Bookshelf.tsx
@@ -5,6 +5,7 @@ import { Html, useGLTF } from '@react-three/drei';
 import { Suspense, useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 import ObjectHint from './ObjectHint';
+import { playCameraWhoosh } from '@/utils/audio';
 
 interface BookshelfProps {
     lampOn: boolean;
@@ -51,6 +52,7 @@ export default function Bookshelf({ lampOn, isBookshelfOpen, onBookshelfClick, i
 
     const handleClick = () => {
         if (!lampOn) return;
+        setTimeout(() => playCameraWhoosh(), 160);
         setHovered(false);
         document.body.style.cursor = 'auto';
         onBookshelfClick();
